30edc0a5f66671d15f8f649120821b9a0684f5b2
Jesse Drebin and jared lutz <jesse@>
2018-03-22 10:50:42 -0700

    add button function toprompt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $ (document).ready(function(){
-  var votingprompt = parseInt(prompt ("Please enter your age. You must be 18 or older to vote.)"));
-  if(votingprompt >= 18){
-    $("#old-farts").show();
-  } else {
-    $("#minors").show();
-  }
+  $("button#register").click(function(event) {
+
+    if (parseInt(prompt("enter your age")) >= 18){
+      $("#old-farts").show();
+    } else {
+      $("#minors").show();
+    }
+  });
+
 });
